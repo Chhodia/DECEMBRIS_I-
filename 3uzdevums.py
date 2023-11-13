fe2a97ac5b11bb44585b49ce4db2df8d72970d0c
Chhodia <97937732+Chhodia@users.noreply.github.com>
2023-11-13 09:28:35 +0200

    Add files via upload

---
+++ 3uzdevums.py
@@ -0,0 +1,11 @@
+number = (input("number: "))
+if number == 0:
+    print('skaitlis ir 0')
+elif number < 0:
+    print("skaitlis ir negatīvs")
+elif number % 2 == 0:
+    print(number, " ir pāru")
+elif number % 2 != 0:
+    print(number, " ir nepāru")
+else:
+    print("ludzu ievadiet SKAITLI!")
